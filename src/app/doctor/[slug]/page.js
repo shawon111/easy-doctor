d39bec370b7bf4d1b15b7783a78f411d5b5f4125
shawon111 <shawonahmmed08509@gmail.com>
2026-06-26 01:24:08 +0600

    isr in doctors

--- src/app/doctor/[slug]/page.js
+++ src/app/doctor/[slug]/page.js
@@ -1,14 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { connectDB } from "@/config/database";
+import { getDoctorsList, getUserBySlug } from "@/services/user.service";
+import Link from "next/link";
 
-export const generateStaticParams = async() => {
-    
+export const revalidate = 60 * 15;
+
+export const generateStaticParams = async () => {
+    await connectDB();
+    const doctors = await getDoctorsList() ?? [];
+    return doctors.map((doctor) => ({
+        slug: doctor?.slug
+    }))
 }
 
-const DoctorHomePage = () => {
+const DoctorHomePage = async({ params }) => {
+    await connectDB();
+    const { slug } = await params;
+    const doctor = await getUserBySlug(slug);
     return (
         <div>
-            
+            <h1 className="">
+               Doctor Name: {doctor?.name}
+            </h1>
+            <p>
+                Bio: {doctor?.bio}
+            </p>
+            <Button type="button">
+                Call: <Link href={`tel:${doctor?.phone}`}>{doctor?.phone}</Link>
+            </Button>
         </div>
     );
 };
 
-export default DoctorPage;
+export default DoctorHomePage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,26 @@
+import { connectDB } from "@/config/database";
+import { getDoctorsList } from "@/services/user.service";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  await connectDB();
+  const doctors = await getDoctorsList() ?? [];
   return (
     <>
       <div className="flex flex-col items-center justify-center h-full">
         <h1 className="text-4xl font-bold mb-4">Welcome to the Doctor App</h1>
         <p className="text-gray-600 mb-8">create your doctor website</p>
+        <ul>
+          {
+            doctors.map((doctor, index) => {
+              return (
+                <li key={doctor._id}>
+                  <Link href={`/doctor/${doctor.slug}`}>Doctor {index + 1}</Link>
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
     </>
   );
