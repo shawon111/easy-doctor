46d28a445c40762c3ceeff0c8575c783d767f8b7
shawon111 <shawonahmmed08509@gmail.com>
2026-08-12 13:56:25 +0600

    SSg on all the pages

--- src/app/doctor/[slug]/layout.js
+++ src/app/doctor/[slug]/layout.js
@@ -1,9 +1,18 @@
+import { connectDB } from "@/config/database";
+import { getDoctorsList } from "@/services/user.service";
+
+export const revalidate = 900;
+
+export const generateStaticParams = async () => {
+  await connectDB();
+  const doctors = (await getDoctorsList()) ?? [];
+  return doctors.map((doctor) => ({
+    slug: doctor?.slug,
+  }));
+};
+
 const DoctorLayout = async ({ children }) => {
-    return (
-        <div>
-            {children}
-        </div>
-    );
+  return <div>{children}</div>;
 };
 
 export default DoctorLayout;
--- src/app/doctor/[slug]/page.js
+++ src/app/doctor/[slug]/page.js
@@ -1,16 +1,4 @@
 import TemplateTwoDarkPageRenderer from "@/components/templates/template-two-dark/TemplateTwoDarkPageRenderer";
-import { connectDB } from "@/config/database";
-import { getDoctorsList } from "@/services/user.service";
-
-export const revalidate = 900;
-
-export const generateStaticParams = async () => {
-  await connectDB();
-  const doctors = (await getDoctorsList()) ?? [];
-  return doctors.map((doctor) => ({
-    slug: doctor?.slug,
-  }));
-};
 
 const pageName = "home";
 
