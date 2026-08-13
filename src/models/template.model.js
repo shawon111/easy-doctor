a84518c59602bb940ebee87d0f9adafd108af554
shawon111 <shawonahmmed08509@gmail.com>
2026-08-14 02:29:43 +0600

    template schema updated to match the content

--- src/models/template.model.js
+++ src/models/template.model.js
@@ -1,23 +1,34 @@
 import mongoose from "mongoose";
 
 const templateSchema = new mongoose.Schema(
-  {
-    type: {
-      type: String,
-      required: true,
-      unique: true,
-      enum: ["template-one", "template-two", "template-three"],
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            index: true,
+        },
+        type: {
+            type: String,
+            required: true,
+            unique: true,
+            enum: ["template-one", "template-two", "template-three"],
+        },
+        variant: {
+            type: String,
+            enum: ["light", "dark"],
+            default: "light",
+        },
+        contentType: {
+            type: String,
+            enum: ["TemplateOneContent", "TemplateTwoContent", "TemplateThreeContent"],
+        },
+        content: {
+            type: mongoose.Schema.Types.ObjectId,
+            refPath: "contentType",
+        },
     },
-    variant: {
-      type: String,
-      enum: ["light", "dark"],
-      default: "light",
-    },
-    content: {
-      type: mongoose.Schema.Types.ObjectId,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 const Template = mongoose.models.Template || mongoose.model("Template", templateSchema);
